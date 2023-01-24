bc7b193d297134ecd7333c8d20039a21ebefbe19
Peipi98 <39802340+Peipi98@users.noreply.github.com>
2023-01-24 14:58:18 +0100

    add prova_TSN.py

---
+++ prova_TSN.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+
+# Define the classifier
+class TSNClassifier(nn.Module):
+    def __init__(self, num_classes):
+        super(TSNClassifier, self).__init__()
+        self.fc = nn.Linear(5120, num_classes)
+
+    def forward(self, x):
+        # Concatenate the features from all segments
+        x = x.view(1, -1)
+        # Pass the concatenated features through a fully connected layer
+        x = self.fc(x)
+        return x
+
+# Initialize the classifier
+num_classes = 10  # Example value, replace with the number of classes in your dataset
+model = TSNClassifier(num_classes)
+
+# Define loss function and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
+
+# Training loop
+for i in range(num_epochs):
+    # Extract features for each segment
+    segment_features = [extract_features(segment) for segment in segments]  # replace with your feature extraction method
+    # Concatenate the features from all segments
+    concatenated_features = torch.cat(segment_features, dim=1)
+    # Forward pass
+    output = model(concatenated_features)
+    # Compute loss
+    loss = criterion(output, labels)
+    # Backward pass and optimization
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
