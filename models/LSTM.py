ec495a61c9c3c9b1c7b5e66f372d06bf50d8db72
FrancescoSorrentino <fra.sorrentino@studenti.polito.it>
2023-02-02 19:47:47 +0100

    updating RNNClassifier

--- models/LSTM.py
+++ models/LSTM.py
@@ -6,19 +6,15 @@ import numpy as np
 class LSTMClassifier(nn.Module):
     def __init__(self, num_classes=8):
         super(LSTMClassifier ,self).__init__()
-        self.lstm = nn.LSTM(input_size=1024, hidden_size=256, num_layers=3)
+        self.lstm = nn.GRU(input_size=1024, hidden_size=256, num_layers=1)
         self.fc1 = nn.Linear(256, 128)
         self.fc2 = nn.Linear(128, num_classes)
         
 
     def forward(self, x):
-        hidden = None
-        y = []
-        for t in range(x.size(1)):
-            out, hidden = self.lstm(x[:,t,:].unsqueeze(1), hidden) 
-            out = self.fc(out[:, -1, :])
-            out = F.relu(out)
-            y.append(self.fc2(out))
-        x = torch.Tensor(np.array(y))
-        x = torch.avg(x, dim=0)
+        out, _ = self.lstm(x) 
+        out = self.fc1(out[:, -1, :])
+        x = F.relu(out)
+        x = self.fc2(x)
+        
         return x, None
