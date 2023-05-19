b68cbcfcef3800db4d364f0d6edc9634a8095b55
Peipi98 <39802340+Peipi98@users.noreply.github.com>
2023-05-19 18:09:16 +0200

    clean repo

--- normalize.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def normalize_tensor(tensor):
-    min_value = np.min(tensor)
-    max_value = np.max(tensor)
-    normalized_tensor = (tensor - min_value) / (max_value - min_value) * 2 - 1
-    return normalized_tensor
